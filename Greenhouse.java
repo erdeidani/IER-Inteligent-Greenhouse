fd93b01058a18650b0fbe980bce017c27b7ebdc2
rvili20 <100083135+rvili20@users.noreply.github.com>
2023-05-25 13:53:03 +0200

    eclipse project added
    project right-click -> properties-> run/debug configurations -> new configuration, main class should be jason.infra.local.RunLocalMAS -> arguments -> program arguments -> write intelligentGreenhouse.mas2j
    this is the config file for the project

---
+++ Greenhouse.java
@@ -0,0 +1,40 @@
+// Environment code for project intelligentGreenhouse.mas2j
+
+import jason.asSyntax.*;
+import jason.environment.*;
+import jason.asSyntax.parser.*;
+
+import java.util.logging.*;
+
+public class Greenhouse extends Environment {
+
+    private Logger logger = Logger.getLogger("intelligentGreenhouse.mas2j."+Greenhouse.class.getName());
+
+    /** Called before the MAS execution with the args informed in .mas2j */
+    @Override
+    public void init(String[] args) {
+        super.init(args);
+        try {
+			addPercept(ASSyntax.parseLiteral("percept(demo)"));
+			
+		} catch (ParseException e) {
+			e.printStackTrace();
+		}
+    }
+
+    @Override
+    public boolean executeAction(String agName, Structure action) {
+        logger.info("executing: "+action+", but not implemented!");
+        if (true) { // you may improve this condition
+             informAgsEnvironmentChanged();
+        }
+        return true; // the action was executed with success
+    }
+
+    /** Called before the end of MAS execution */
+    @Override
+    public void stop() {
+        super.stop();
+    }
+}
+
